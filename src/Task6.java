94c915d9e07d8b7b586d71dbcdb476fd542a13f1
Iryna Yelunina <lucky.asterisk@gmail.com>
2023-09-08 23:13:56 +0200

    tasks new

---
+++ src/Task6.java
@@ -0,0 +1,34 @@
+public class Task6 {
+    /*
+    Практика
+! В каждой задаче все значения могут меняться, и расчет должен быть произведен в любом случае.
+Задача 6*: Калькулятор криптовалюты для Васи (CryptoCalculator)
+Описание:
+У Васи есть 12350 монет криптовалюты. Все монеты он купил по цене 0.27 евро. Сегодня монета стоит 16.38 евро.
+
+Вопросы:
+На какую сумму Вася может рассчитывать, если продаст все свои монеты?
+Имя метода: calculateTotalValue
+Какая прибыль будет у Васи, если он продаст монеты сейчас?
+Имя метода: calculateProfit
+Сколько монет нужно продать, чтобы получить x прибыль?
+Имя метода: coinsForXProfit
+Какая стоимость монеты должна быть, чтобы получить x прибыль?
+Имя метода: priceForXProfit
+Сколько монет нужно по текущему курсу, чтобы получить x сумму?
+Имя метода: coinsForXAmount
+Cо звёздочкой *:
+Всё то же самое, только любой доход облагается налогом. Налог 23%.
+Метод: applyTax
+Ожидаемый вывод:
+Программа должна быть готова к тому, что все значения могут меняться, и расчёт должен быть произведён в любом случае.
+
+Подсказки:
+double calculateTotalValue(double initialCoins, double currentPrice)
+double calculateProfit(double initialCoins, double initialPrice, double currentPrice)
+double coinsForXProfit(double initialCoins, double initialPrice, double currentPrice, double desiredProfit)
+double priceForXProfit(double initialCoins, double initialPrice, double desiredProfit)
+double coinsForXAmount(double currentPrice, double desiredAmount)
+double applyTax(double income)
+     */
+}
