313d9bb8261bc8492c8c797aace19038c6e9e062
Iryna Yelunina <lucky.asterisk@gmail.com>
2023-09-08 23:26:45 +0200

    task3

--- src/Task3.java
+++ src/Task3.java
@@ -2,10 +2,21 @@ public class Task3 {
     /*
     Задача 3: Расчет расхода топлива (FuelCalculation)
 Описание
-У вас есть автомобиль, который расходует N литров топлива на 100 км. Вам нужно рассчитать, сколько топлива потребуется для поездки на расстояние M км. (calculateFuelConsumption)
+У вас есть автомобиль, который расходует N литров топлива на 100 км. Вам нужно рассчитать,
+сколько топлива потребуется для поездки на расстояние M км. (calculateFuelConsumption)
 
 Ожидаемый вывод
 Расход топлива для 150 км при расходе 8 л/100 км.
 Расход топлива для 300 км при расходе 10 л/100 км.
      */
+    public static void main(String[] args) {
+        double result = calculateFuelConsumption(8, 100, 150);
+        System.out.println(result);
+        result = calculateFuelConsumption(10, 100, 300);
+        System.out.println(result);
+    }
+
+    public static double calculateFuelConsumption(double l, double km, double n) {
+        return l / km * n;
+    }
 }
